d151aeb3925fa44bf84b7a95b09bf9c3f814f699
Bashir Umar Liman <limbashcomty@gmail.com>
2024-12-24 16:56:08 +0100

    Updated lib folders and added assets folder

---
+++ lib/src/utils/themes/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class THealthThemes {
+  THealthThemes._();
+
+  static ThemeData lightMode = ThemeData(
+    brightness: Brightness.light,
+  );
+  static ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+  );
+}
---
+++ lib/src/utils/themes/widget themes/text_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class THealthTextTheme {
+  THealthTextTheme._();
+
+  static TextTheme lightMode = TextTheme();
+  static TextTheme darkMode = TextTheme();
+}
